a2c6b950498e32cc7a1760873ca02b7576316f3e
Yeonwoo Lee <ylee100@u.rochester.edu>
2018-03-16 18:33:00 -0400

    Received Room Info from Server

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,14 @@
 import $ from "jquery"
+import io from 'socket.io-client'
+
+const socket = io ({
+  transports:['websocket']
+})
+
+socket.on('rooms', (data)=>{
+  createtable(data);
+})
+
 function createtable(arr) {
     for (var j = 0; j < arr.length; j++) {
             $("#tab").append("<tr><td>" + arr[j] + " </td></tr>");
@@ -7,6 +17,4 @@ function createtable(arr) {
 
 $( document ).ready(function() {
     console.log( "ready!" );
-    var arr = [1,2,3]
-    createtable(arr);
 });
