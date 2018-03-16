391ecabca14bd17e7af528416d008d98b78b5aa3
Yeonwoo Lee <ylee100@u.rochester.edu>
2018-03-16 18:52:36 -0400

    Send Room Selection to Server

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,20 +1,25 @@
-import $ from "jquery"
+import $ from 'jquery'
 import io from 'socket.io-client'
 
-const socket = io ({
-  transports:['websocket']
+const socket = io({
+  transports: ['websocket']
 })
 
-socket.on('rooms', (data)=>{
-  createtable(data);
+socket.on('rooms', (data) => {
+  createtable(data)
 })
 
-function createtable(arr) {
-    for (var j = 0; j < arr.length; j++) {
-            $("#tab").append("<tr><td>" + arr[j] + " </td></tr>");
-    }
+function createtable (arr) {
+  for (var j = 0; j < arr.length; j++) {
+    $('#tab').append('<tr><td>' + arr[j] + ' </td></tr>')
+  }
 }
 
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
+$("roomForm").submit(){
+  event.preventDefault()
+  socket.emit('create', $("selectedRoom").innerHTML)
+}
+
+$(document).ready(function () {
+  console.log('ready!')
+})
