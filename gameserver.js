ef950d3aff843a1dbcb3ba160b791fe430a64982
Donghyeon Kim <donghyeon@tacocube.com>
2018-03-16 19:19:06 -0400

    Add onCreate Function

--- gameserver.js
+++ gameserver.js
@@ -7,9 +7,24 @@ const io = require('socket.io')(server, {
 
 var rooms = []
 
+function onCreate (data, socket) {
+  // Check if the requested name already exists on the server.
+  if(rooms.includes(data)) {
+    // Room already exists.
+    socket.emit('create', {success: false, roomname: ''})
+  } else {
+    // Room name is available.
+    rooms.push(data)
+    socket.emit('create', {success: true, roomname: data})
+    console.log('Room ' + data + ' is created.')
+  }
+}
+
 function onConnect (socket) {
   // Emit the list of rooms on server.
   socket.emit('rooms', rooms)
+
+  socket.on('create', (data) => onCreate(data, socket))
 }
 
 export var startServer = function (port) {
