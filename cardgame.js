0ff67078cff5e9f1685f5800dde963dab2a4fd73
Donghyeon Kim <donghyeon@tacocube.com>
2018-03-16 20:47:29 -0400

    Add createCard Function

--- cardgame.js
+++ cardgame.js
@@ -1,3 +1,28 @@
+function createCard (suit, rank) {
+  return {suit: suit, rank: rank}
+}
+
+function createSuit (suit) {
+  var ret = []
+  for (var i = 2; i <= 10; i++) {
+    ret.push(createCard(suit, i.toString()))
+  }
+  ret.push(createCard(suit, 'J'))
+  ret.push(createCard(suit, 'Q'))
+  ret.push(createCard(suit, 'K'))
+  ret.push(createCard(suit, 'A'))
+
+  return ret
+}
+function createDeck () {
+  var ret = []
+  ret.push(createCard('joker', ''))
+  ret.push(createCard('joker', ''))
+  ret = ret.concat(createSuit('hearts'), createSuit('diamonds'), createSuit('clubs'), createSuit('spades'))
+
+  return ret
+}
+
 export var createGame = function () {
   // TODO create game object and return.
   return {}
