fac40be4b58caf809c6bb51d9659c2f3cacd4f44
Donghyeon Kim <donghyeon@tacocube.com>
2018-03-16 20:02:07 -0400

    Generate URL on Create Room Request and Notify All Users On Server

--- gameserver.js
+++ gameserver.js
@@ -14,8 +14,13 @@ function onCreate (data, socket) {
     socket.emit('create', {success: false, url: ''})
   } else {
     // Room name is available.
+    // Add the room name to the array.
     rooms.push(data)
+    // Response with the room url.
     socket.emit('create', {success: true, url: '/room/' + data})
+
+    // Notify all users on the server that a new room is created.
+    socket.broadcast.emit('rooms', rooms)
     console.log('Room ' + data + ' is created.')
   }
 }
@@ -30,6 +35,9 @@ function onConnect (socket) {
 exports.startServer = function (port) {
   // Statically serve frontend.
   app.use(express.static('frontend/dist'))
+  app.get('/room/:roomName', function (req, res, next) {
+    res.sendFile('frontend/dist/room.html', {root: __dirname})
+  })
 
   io.on('connect', onConnect)
   server.listen(port, () => console.log('Card game server listening on port ' + port))
