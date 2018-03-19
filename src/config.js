8a250e1d23e0526d82f492faf330a9fcc74a73c7
Donghyeon Kim <donghyeon@tacocube.com>
2018-03-19 14:13:22 -0400

    Add Namespace for Actual Gameplay

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   get CREATE_CHANNEL () { return 'create' },
   get JOIN_CHANNEL () { return 'join' },
-  get ROOMS_CHANNEL () { return 'rooms' }
+  get ROOMS_CHANNEL () { return 'rooms' },
+  get MATCH_NAMESPACE () { return 'match' }
 }
